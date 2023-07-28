4b7549390d45eca6a50c4af980badd293e99499b
bakhacode <bakhamitai@naver.com>
2023-07-28 10:56:25 +0900

    feat: 레이블 페이지 UI 및 기능 구현
    - 레이블 페이지와 레이블들에 대한 설명 추가 생성을 위한 컴포넌트 생성
    - 과도한 드릴링과 간편한 상태관리를 위해 레이블 컨텍스트 사용

--- fe/src/pages/LabelPage.tsx
+++ fe/src/pages/LabelPage.tsx
@@ -3,44 +3,57 @@ import { Background } from "../components/common/Background";
 import { TabButton } from "../components/common/TabButton";
 import { useState } from "react";
 import { Button } from "../components/common/Button";
-import { color } from "../constants/colors";
+// import { color } from "../constants/colors";
 import { fonts } from "../components/util/Txt";
+import { LabelDetail } from "../components/Label/LabelDetail";
+import { LabelElement } from "../components/Label/LabelElement";
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "@emotion/react";
+import { ColorScheme } from "../contexts/ThemeContext";
+import { Alert } from "../components/util/Alert";
+
+export type LabelType = {
+  id: number;
+  title: string;
+  description: string;
+  backgroundColor: string;
+  isDark: boolean;
+};
 
 export function LabelPage() {
-  const labelList = [
-    {
-      id: 1,
-      title: "Label",
-      description: "레이블1 설명",
-      color: "#FEFEFE",
-    },
-    {
-      id: 2,
-      title: "documentation",
-      description: "레이블2 설명",
-      color: "#0025E6",
-    },
-    {
-      id: 3,
-      title: "bug",
-      description: "레이블3 설명",
-      color: "#FF3B30",
-    },
-  ];
-
-  const leftText = "레이블";
-  const rightText = "마일스톤";
-  const labelCount = labelList.length;
-  const milestoneCount = 2;
-
+  const color = useTheme() as ColorScheme;
+  const navigate = useNavigate();
   const [isLeftSelected, setIsLeftSelected] = useState(true);
+  const [isAddLabelOpen, setIsAddLabelOpen] = useState(false);
+  const AddLabelButtonStatus = isAddLabelOpen ? "disabled" : "enabled";
+
+  const onClickAddLabelButton = () => {
+    setIsAddLabelOpen(true);
+  };
+  const onClickCompleteButton = () => {
+    setIsAddLabelOpen(false);
+  };
+  const onClickCancelButton = () => {
+    setIsAddLabelOpen(false);
+  };
+
+  const onClickMilestoneButton = () => {
+    console.log("마일스톤 버튼 클릭");
+    navigate("/milestone");
+  };
 
   const onClickLeftTab = () => {
     setIsLeftSelected(true);
   };
   const onClickRightTab = () => {
     setIsLeftSelected(false);
+    onClickMilestoneButton();
   };
+
+  const leftText = "레이블";
+  const rightText = "마일스톤";
+  const labelCount = labelList.length;
+  const milestoneCount = 2;
 
   const leftTabProps = {
     leftIcon: "label",
@@ -71,113 +84,85 @@ export function LabelPage() {
             leftTabProps={leftTabProps}
             rightTabProps={rightTabProps}
           />
-          <Button
-            type="contained"
-            size="S"
-            text="레이블 추가"
-            icon="plus"
-            flexible="fixed"
-          />
+          <div className="addLabelButton" onClick={onClickAddLabelButton}>
+            <Button
+              status={AddLabelButtonStatus}
+              type="contained"
+              size="S"
+              text="레이블 추가"
+              icon="plus"
+            />
+          </div>
         </div>
         <div
           css={{
-            position: "relative",
-            top: "24px",
             display: "flex",
             flexDirection: "column",
-            borderRadius: "16px",
-            border: `1px solid ${color.neutral.border.default}`,
-            overflow: "hidden",
+            gap: "24px",
+            position: "relative",
+            top: "24px",
           }}>
+          {isAddLabelOpen ? (
+            <LabelDetail
+              mode="add"
+              onClickCompleteButton={onClickCompleteButton}
+              onClickCancelButton={onClickCancelButton}
+            />
+          ) : null}
           <div
             css={{
+              position: "relative",
+              // top: "24px",
               display: "flex",
-              alignItems: "center",
-              padding: "0 32px",
-              boxSizing: "border-box",
-              height: "64px",
-              backgroundColor: color.neutral.surface.default,
-              ...fonts.bold16,
+              flexDirection: "column",
+              borderRadius: "16px",
+              border: `1px solid ${color.neutral.border.default}`,
+              overflow: "hidden",
             }}>
-            {labelCount}개의 레이블
+            <div
+              css={{
+                display: "flex",
+                alignItems: "center",
+                padding: "0 32px",
+                boxSizing: "border-box",
+                height: "64px",
+                color: color.neutral.text.default,
+                backgroundColor: color.neutral.surface.default,
+                ...fonts.bold16,
+              }}>
+              {labelCount}개의 레이블
+            </div>
+            {labelList.map((label) => {
+              return <LabelElement key={label.id} label={label} />;
+            })}
           </div>
-          {labelList.map((label) => {
-            return (
-              <div
-                css={{
-                  display: "flex",
-                  justifyContent: "space-evenly",
-                  alignItems: "center",
-                  gap: "32px",
-                  width: "100%",
-                  height: "96px",
-                  backgroundColor: color.neutral.surface.strong,
-                  borderTop: `1px solid ${color.neutral.border.default}`,
-                  ...fonts.medium12,
-                }}>
-                <div
-                  className="labelWrapper"
-                  css={{ width: "178px", height: "24px" }}>
-                  <div
-                    className="label"
-                    css={{
-                      border:
-                        label.color === "#FEFEFE"
-                          ? `1px solid ${color.neutral.border.default}`
-                          : "none",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      borderRadius: "16px",
-                      padding: "0px 12px",
-                      boxSizing: "border-box",
-                      backgroundColor: label.color,
-                      width: "max-content",
-                      height: "24px",
-                      whiteSpace: "nowrap",
-                      ...fonts.medium12,
-                      color:
-                        label.color === "#FEFEFE"
-                          ? color.neutral.text.weak
-                          : color.brand.text.default,
-                    }}>
-                    {label.title}
-                  </div>
-                </div>
-                <div
-                  className="description"
-                  css={{
-                    width: "870px",
-                    height: "24px",
-                    color: color.neutral.text.weak,
-                    ...fonts.medium16,
-                  }}>
-                  {label.description}
-                </div>
-                <div
-                  className="buttonTab"
-                  css={{ display: "flex", width: "106px", height: "32px" }}>
-                  <Button
-                    icon="edit"
-                    type="ghost"
-                    size="S"
-                    text="편집"
-                    flexible="fixed"
-                  />
-                  <Button
-                    icon="trash"
-                    type="ghost"
-                    size="S"
-                    text="삭제"
-                    flexible="fixed"
-                    textColor={color.danger.text.default}
-                  />
-                </div>
-              </div>
-            );
-          })}
         </div>
       </div>
+      <Alert text="정말 이 레이블을 삭제하시겠습니까?" />
     </Background>
   );
 }
+
+const labelList = [
+  {
+    id: 1,
+    title: "Label",
+    description: "레이블1 설명",
+    backgroundColor: "#FEFEFE",
+    isDark: true,
+  },
+  {
+    id: 2,
+    title: "documentation",
+    description: "레이블2 설명",
+    backgroundColor: "#0025E6",
+    isDark: false,
+  },
+  {
+    id: 3,
+    title: "bug",
+    description: "레이블3 설명",
+    backgroundColor: "#FF3B30",
+    isDark: false,
+  },
+];
