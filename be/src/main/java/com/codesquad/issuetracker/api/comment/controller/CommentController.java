079980ea8e088499373f8067ed21021359929457
CDBchan <kdjin1215@naver.com>
2023-08-01 13:26:21 +0900

    feat: comment 작성기능 구현

---
+++ be/src/main/java/com/codesquad/issuetracker/api/comment/controller/CommentController.java
@@ -0,0 +1,26 @@
+package com.codesquad.issuetracker.api.comment.controller;
+
+import com.codesquad.issuetracker.api.comment.dto.request.CommentCreateRequest;
+import com.codesquad.issuetracker.api.comment.service.CommentService;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/api/{organizationTitle}/issues/{issueId}/comments")
+    public ResponseEntity<Map<String, Long>> create(@PathVariable Long issueId,
+        @RequestBody CommentCreateRequest commentCreateRequest) {
+        Long commentId = commentService.create(issueId, commentCreateRequest);
+        return ResponseEntity.ok()
+            .body(Map.of("id", commentId));
+    }
+}
