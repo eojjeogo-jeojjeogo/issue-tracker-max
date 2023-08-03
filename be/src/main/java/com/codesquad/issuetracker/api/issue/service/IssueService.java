50ae2759c242a761a03493025c644f321be9a552
jinny-l <jinny.l.dev@gmail.com>
2023-08-03 13:46:25 +0900

    feat: 이슈 삭제 기능 구현
    * 이슈 삭제 시, 해당 이슈의 마일스톤/코멘트/라벨/담당자 정보 삭제

--- be/src/main/java/com/codesquad/issuetracker/api/issue/service/IssueService.java
+++ be/src/main/java/com/codesquad/issuetracker/api/issue/service/IssueService.java
@@ -55,4 +55,9 @@ public class IssueService {
         issueRepository.save(issueLabels);
     }
 
+    @Transactional
+    public void deleteOne(Long issueId) {
+        issueRepository.delete(issueId);
+        commentRepository.delete(issueId);
+    }
 }
