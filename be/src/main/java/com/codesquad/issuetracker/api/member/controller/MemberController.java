ccef13f9090a17d7ae97b23a7d63465cc5856ee9
CDBchan <kdjin1215@naver.com>
2023-08-09 23:01:21 +0900

    feat: local 회원가입 기능 구현

--- be/src/main/java/com/codesquad/issuetracker/api/member/controller/MemberController.java
+++ be/src/main/java/com/codesquad/issuetracker/api/member/controller/MemberController.java
@@ -2,7 +2,10 @@ package com.codesquad.issuetracker.api.member.controller;
 
 import com.codesquad.issuetracker.api.member.Service.MemberService;
 import com.codesquad.issuetracker.api.member.dto.OauthSignInRequest;
+import com.codesquad.issuetracker.api.member.dto.SignUpRequest;
 import com.codesquad.issuetracker.api.oauth.dto.OauthSignInResponse;
+import java.util.Collections;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,5 +28,13 @@ public class MemberController {
         );
         return ResponseEntity.ok()
                 .body(oAuthSignInResponse);
+    }
+
+    @PostMapping("/api/sign-up/{provider}")
+    public ResponseEntity<Map<String, Long>> signUp(@RequestBody SignUpRequest signUpRequest,
+                                                    @PathVariable String provider) {
+        Long memberId = memberService.signUp(signUpRequest, provider);
+        return ResponseEntity.ok()
+                .body(Collections.singletonMap("id", memberId));
     }
 }
