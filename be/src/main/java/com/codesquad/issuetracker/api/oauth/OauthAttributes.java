1afa36ff11aa40f06632280cb5f6ee1d8db2be50
CDBchan <kdjin1215@naver.com>
2023-08-09 22:02:01 +0900

    refactor: access_tocken 을 통해 정보를 받아올때 필요없는 부분 삭제

--- be/src/main/java/com/codesquad/issuetracker/api/oauth/OauthAttributes.java
+++ be/src/main/java/com/codesquad/issuetracker/api/oauth/OauthAttributes.java
@@ -9,7 +9,6 @@ public enum OauthAttributes {
         @Override
         public UserProfile of(Map<String, Object> attributes) {
             return UserProfile.builder()
-                    .oauthId(String.valueOf(attributes.get("id")))
                     .email((String) attributes.get("email"))
                     .name((String) attributes.get("name"))
                     .imageUrl((String) attributes.get("avatar_url"))
--- be/src/main/java/com/codesquad/issuetracker/api/oauth/dto/UserProfile.java
+++ be/src/main/java/com/codesquad/issuetracker/api/oauth/dto/UserProfile.java
@@ -5,14 +5,12 @@ import lombok.Getter;
 
 @Getter
 public class UserProfile {
-    private final String oauthId;
     private final String email;
     private final String name;
     private final String imageUrl;
 
     @Builder
-    public UserProfile(String oauthId, String email, String name, String imageUrl) {
-        this.oauthId = oauthId;
+    public UserProfile(String email, String name, String imageUrl) {
         this.email = email;
         this.name = name;
         this.imageUrl = imageUrl;
